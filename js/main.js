3927e80381cbb232bc29207a1576a3f8e01d106e
Yuriy Chaban <Yuriy Chaban>
2016-11-26 17:05:52 +0200

    add mobile version home page and fix desctop version home page

--- js/main.js
+++ js/main.js
@@ -10,6 +10,18 @@ $(function () {
     });
 });
 
+// for main menu desctop
+$(function () {
+    $('.main-menu .list-menu li a').each(function () {
+        var location = window.location.pathname;
+        var link = this.pathname;
+        var result = location.match(link);
+        if(result && link.length > 0) {
+            $(this).addClass('main-menu-active');
+        }
+    });
+});
+
 // for services-more page services item
 $(function () {
     $('.services-section .services-list li a').each(function () {
@@ -21,6 +33,7 @@ $(function () {
         }
     });
 });
+
 
 // for footer menu
 $(function () {
@@ -46,51 +59,69 @@ $(function () {
     });
 });
 
-
-// for slider in Home page
-var swiper_main = new Swiper('.swiper-container-home-page', {
-    paginationClickable: true,
-    nextButton: '.swiper-button-next',
-    prevButton: '.swiper-button-prev',
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 0,
-    simulateTouch: true,
-    centeredSlides: true,
-    autoplay: 3500,
-    speed: 1000,
-    watchSlidesProgress: true,
-    autoplayDisableOnInteraction: false
+// FOR SLIDER MORE 
+$(document).ready(function() {
+    $('.center').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 2
+        }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 1
+    }
+}
+],
+nextArrow: '<img class="prev-btn" src="images/more-prev.png" alt="more prev">',
+prevArrow: '<img class="next-btn" src="images/more-next.png" alt="more next">',
+});
 });
 
 
+// TEXT SLIDER ON MAIN PAGE
+$(document).ready(function() {
+    $('.fade').slick({
+      dots: false,
+      infinite: true,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      speed: 500,
+      fade: true,
+      centerPadding: '10px',
+      cssEase: 'linear',
+      nextArrow: '<div class="next-btn"></div>',
+      prevArrow: '<div class="prev-btn"></div>'
+  });
+});
 
-// FOR SLIDER MORE 
-$('.center').slick({
-    infinite: true,
-    speed: 300,
-    slidesToShow: 2,
-    slidesToScroll: 1,
-    responsive: [
-        {
-          breakpoint: 768,
-              settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 2
-            }
-        },
-        {
-          breakpoint: 480,
-              settings: {
-                arrows: false,
-                centerMode: true,
-                centerPadding: '40px',
-                slidesToShow: 1
-            }
-        }
-    ],
-nextArrow: '<img class="prev-btn" src="images/more-prev.png" alt="more prev">',
-prevArrow: '<img class="next-btn" src="images/more-next.png" alt="more next">',
+// for go to block for id
+$(document).ready(function(){
+    $(function(){
+        $('.quote-btn').click(function() {
+            if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'')
+                && location.hostname == this.hostname) {
+                var $target = $(this.hash);
+            $target = $target.length && $target || $('[name=' + this.hash.slice(1) +']');
+            if ($target.length) {
+        var targetOffset = $target.offset().top - ($("#quote-block").outerHeight(true)- 590); //#main-header - заменить на ваш элемент
+        $('html,body').animate({scrollTop: targetOffset}, 1000);
+        return false;
+    }
+}
+});
+    });
 });
