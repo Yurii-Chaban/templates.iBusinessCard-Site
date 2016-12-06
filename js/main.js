8ed87da9305f449cd6523b0fbc298a98ce461d4b
Yuriy Chaban <Yuriy Chaban>
2016-12-06 21:53:02 +0200

    fix sticky header

--- js/main.js
+++ js/main.js
@@ -143,12 +143,6 @@ $(document).ready(function() {
       'wrapAround': true
   })
 });
-// slick navigation
-$(document).ready(function() {
-    $(function(){
-        $('#sticky-header').stickMe();
-    });
-});
 $(document).ready(function() {
     $('.ui.dropdown')
     .dropdown();
